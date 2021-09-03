d71608abddbcb8f7e1cfac66b76a03754a1313d5
Bo Yang Li <lby10@hotmail.com>
2021-09-03 01:19:52 -0500

    Completed password validation program and updated README.md

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
 const validatePassword = require('./passwordvalidation'); //import the function
 
 // Your code goes here!
+
+const readline = require('readline');
+
+const rl = readline.createInterface({
+	input: process.stdin,
+	output: process.stdout
+});
+
+console.log('Enter a password to validate (or :q to quit)');
+
+let passwordPrompt = () => {
+	rl.question('$ ', userInput => {
+		if (userInput === ':q') {
+			console.log('Goodbye!');
+			rl.close();
+		}
+		else if (validatePassword(userInput)) {
+			console.log(`The password is valid: ${userInput}`);
+			passwordPrompt();
+		}
+		else {
+	                console.log(`The password is invalid: ${userInput}`);
+			passwordPrompt();
+		}
+	});
+};
+
+passwordPrompt();
